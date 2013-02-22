02e58235c503b2d2894c1a91c2806b2c93394c66
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-02-22 21:40:33 +0100

    I am a bit stupid

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -1,6 +1,6 @@
 $URL = ce-bt-chat.meteor.com
 if $TRAVIS_PULL_REQUEST != false then
-  $URL = ce-bt-chat-$TRAVIS_PULL_REQUEST
+  $URL = ce-bt-chat-$TRAVIS_PULL_REQUEST.meteor.com
 fi
 
 meteor deploy $URL

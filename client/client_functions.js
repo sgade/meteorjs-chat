0a59e61a48c33113ae140eb33602ae3c0c84a2ca
Sören Gade <gade.soeren@googlemail.com>
2014-02-27 17:23:52 +0100

    Fixed client functions.

--- client/client_functions.js
+++ client/client_functions.js
@@ -1,18 +1,19 @@
 if (Meteor.isClient) {
 	var mySound;
-    var lastSoundPlayed = 0;
-		var oldScroll = 0;
-    function scrollMessagesToBottom() {
-			var selector = $(".messages");
-			
-			var newScroll = selector[0].scrollHeight;
-			      
-      selector.scrollTop(oldScroll);
-			selector.animate({scrollTop: newScroll});
-			oldScroll = newScroll;
-    }
+  var lastSoundPlayed = 0;
+	var oldScroll = 0;
+  
+  scrollMessagesToBottom = function() {
+	  var selector = $(".messages");
+		
+		var newScroll = selector[0].scrollHeight;
+		
+    selector.scrollTop(oldScroll);
+		selector.animate({scrollTop: newScroll});
+		oldScroll = newScroll;
+  }
 	
-	function playMessageReceivedSound() {
+	playMessageReceivedSound = function() {
 		if ( lastSoundPlayed + 500 < new Date().getTime() ) {
             if ( mySound !== undefined )
             {

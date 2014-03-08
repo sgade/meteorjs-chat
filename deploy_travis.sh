a705929330c004b75d7563f6ffa457a30c1b6b52
Jan-Henrik <jh.bruhn97@gmail.com>
2014-03-08 21:37:59 +0100

    Update deploy_travis.sh

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -5,4 +5,4 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   URL=ce-bt-chat-$TRAVIS_PULL_REQUEST.meteor.com
 fi
 
-echo "jh.bruhn@me.com" | meteor deploy $URL
+(sleep 1; echo "jh.bruhn@me.com") | meteor deploy $URL
